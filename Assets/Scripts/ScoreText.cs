1c1fa72091fd9634a86f31eb65fab2e1ebe9d354
alanguo-git <yuxuanguosde@gmail.com>
2021-02-07 00:06:37 -0800

    add score item

---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour
+{
+    Text text;
+    public static int coinAmount;
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = coinAmount.ToString();
+    }
+}
